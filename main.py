59a2532bea235a25a9988c06e33bbf76359d9429
Bonifacio de Oliveira <bldof.eng16@dmz.org.br>
2020-02-07 12:53:11 -0400

    fix current_os error

--- main.py
+++ main.py
@@ -3,23 +3,29 @@ import os
 
 class pnmwifi:
 
-    def __init__(self):
+    def __init__(self, debug_mode=False):
         self.current_os = None  # Current os of host machine
         self.root_user = False  # Allow or disallow use commands as root
         self.get_current_os()  # Set current_os value
+        self.debug_mode = debug_mode
         pass
 
     # List available networks
-    def get_networks(self, debug=False):
-        commands = ['sudo ifconfig wlo1 up && sudo iwlist wlo1 scan | grep ESSID']
+    def get_networks(self, debug_mode=False):
+        commands = ['sudo ifconfig wlo1 up && sudo iwlist wlo1 scan | grep ESSID',
+                    'sudo ifconfig wlan0 up && sudo iwlist wlan0 scan | grep ESSID']
+        networks = []
         if self.current_os == "ubuntu":
-            networks = []
             command_return = os.popen(commands[0]).read()
-            no_processed_return = command_return.split("\"")
-            processed_return = self.list_elements_to_string(no_processed_return)
-            for network in processed_return:
-                if network.find("ESSID") == -1:
-                    networks.append(network)
+        if self.current_os == "raspberrypi":
+            command_return = os.popen(commands[1]).read()
+        if self.debug_mode:
+            print(command_return)
+        no_processed_return = command_return.split("\"")
+        processed_return = self.list_elements_to_string(no_processed_return)
+        for network in processed_return:
+            if network.find("ESSID") == -1 and network.find("\n") == -1:
+                networks.append(network)
 
         return(networks)
 
@@ -36,10 +42,10 @@ class pnmwifi:
     # Get current os running at host machine
     def get_current_os(self):
         os_info = str(os.uname())
-        if(os_info.find("Ubuntu")):
+        if(os_info.find('Ubuntu') != -1):
             self.current_os = "ubuntu"
-        elif(os_info.find("raspberrypi")):
-            self.current_os = "raspibian"
+        if(os_info.find('raspberrypi') != -1):
+            self.current_os = "raspberrypi"
         pass
 
     def list_elements_to_string(self, original_list):
